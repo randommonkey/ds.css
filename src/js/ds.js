6c3f656a81173d627da928472f4954b9c4d07855
David Daza <03dazal@gmail.com>
2018-04-10 13:46:45 -0500

    JS code to manage search component

--- src/js/ds.js
+++ src/js/ds.js
@@ -1,4 +1,4 @@
-/** Class to manage splitted text sections */
+/** Class to manage splitted text components */
 class SplittedText {
   /**
    * Create a SplittedText object
@@ -35,7 +35,7 @@ class SplittedText {
 const splittedTexts = Array.from(document.querySelectorAll('[id^=splitted]'))
 splittedTexts.forEach(sT => new SplittedText(sT).buildDOM())
 
-/** Class to manage banner sections */
+/** Class to manage banner components */
 class Banner {
   /**
    * Create a Banner object
@@ -46,6 +46,7 @@ class Banner {
   }
   /**
    * Reset class and id attributes
+   * @param {*} node - a HTML node
    */
   resetAttr(node) {
     node.removeAttribute('id')
@@ -146,6 +147,69 @@ class Banner {
   }
 }
 
-// Banner
 const banners = Array.from(document.querySelectorAll('[id^=banner]'))
 banners.forEach(banner => new Banner(banner).buildDOM())
+
+/** Class to manage search components */
+class Search {
+  /**
+   * Create a Search object
+   * @param {*} node - a HTML node
+   */
+  constructor(node) {
+    this.node = node
+  }
+  /**
+   * Reset class and id attributes
+   * @param {*} node - a HTML node
+   */
+  resetAttr(node) {
+    node.removeAttribute('id')
+    node.removeAttribute('class')
+  }
+  /**
+   * Set attributes to a give nove
+   * @param {*} node - a HTML node
+   * @param {object} attrs - Object with attribute name and attribute value
+   */
+  setAttr(node, attrs) {
+    const keys = Object.keys(attrs)
+    keys.forEach(key => node.setAttribute(key, attrs[key]))
+  }
+  /**
+   * Add CSS styles to node
+   * @param {*} node - a HTML node
+   * @param {string} property - CSS property name
+   * @param {string} value - CSS property value
+   */
+  css(node, property, value) {
+    node.style[property] = value
+  }
+  /**
+   * Build DOM structure for banner component
+   */
+  buildDOM() {
+    this.node.removeChild(this.node.querySelector('h1'))
+    this.resetAttr(this.node)
+    this.node.classList.add('search')
+
+    const list = this.node.querySelector('ul') || this.node.querySelector('ol')
+    const items = Array.from(list.getElementsByTagName('li'))
+
+    const input = document.createElement('input')
+    this.setAttr(input, { type: 'text', placeholder: 'Buscar' })
+    this.node.insertBefore(input, list)
+
+    input.addEventListener('keyup', (event) => {
+      const filter = event.target.value.toLowerCase()
+      items.forEach(item => {
+        item.textContent.toLowerCase().includes(filter) ?
+          this.css(item, 'display', 'list-item') :
+          this.css(item, 'display', 'none')
+      })
+    })
+  }
+}
+
+const searchs = Array.from(document.querySelectorAll('[id^=search]'))
+searchs.forEach(s => new Search(s).buildDOM())
