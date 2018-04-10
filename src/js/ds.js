ae5dd47fdf208c35cddf9a0abd33728d6ab5ff86
David Daza <03dazal@gmail.com>
2018-04-10 11:39:21 -0500

    Init js code for banner component

--- src/js/ds.js
+++ src/js/ds.js
@@ -2,18 +2,25 @@
 class SplittedText {
   /**
    * Create a SplittedText object
-   * @param {*} node - an HTML node
+   * @param {*} node - a HTML node
    */
   constructor(node) {
     this.node = node
   }
   /**
+   * Reset class and id attributes
+   */
+  resetAttr() {
+    this.node.removeAttribute('id')
+    this.node.removeAttribute('class')
+  }
+  /**
    * Build DOM structure for splitted text component
    */
   buildDOM() {
-    this.node.removeAttribute('id')
+    this.node.removeChild(this.node.querySelector('h1'))
     const classes = this.node.getAttribute('class')
-    this.node.setAttribute('class', '')
+    this.resetAttr()
     const posRegexp = /init_\w*/
     if (classes.match(posRegexp)) {
       const position = classes.match(posRegexp)[0].split('_').pop()
@@ -22,95 +29,123 @@ class SplittedText {
       this.node.classList.add('right')
     }
     this.node.classList.add('splitted-text')
-    this.node.removeChild(this.node.querySelector('h1'))
   }
-
 }
 
 const splittedTexts = Array.from(document.querySelectorAll('[id^=splitted]'))
 splittedTexts.forEach(sT => new SplittedText(sT).buildDOM())
 
-function textToDom (txt) {
-  const parser = new DOMParser()
-  const document = parser.parseFromString(txt, 'text/html')
-  return document.body.innerHTML
+/** Class to manage banner sections */
+class Banner {
+  /**
+   * Create a Banner object
+   * @param {*} node - a HTML node
+   */
+  constructor(node) {
+    this.node = node
+  }
+  /**
+   * Reset class and id attributes
+   */
+  resetAttr(node) {
+    node.removeAttribute('id')
+    node.removeAttribute('class')
+  }
+  /**
+   * Parse text into HTML
+   * @param {string} txt - A text like HTML
+   * @returns {} Text parsed into HTML
+   */
+  textToDOM(txt) {
+    const parser = new DOMParser()
+    const document = parser.parseFromString(txt, 'text/html')
+    return document.body.innerHTML
+  }
+  /**
+   * Get and apply background color from the classes, if any
+   * @param {string} classes
+   */
+  mapBgColor(classes) {
+    const bgRegex = /bg_\w*/
+    if (classes.match(bgRegex)) {
+      const color = classes.match(bgRegex)[0].split('_').pop()
+      this.css('backgroundColor', `#${color}`)
+    }
+  }
+  /**
+   * Get and apply banner size from the clasess, if any
+   * @param {string} classes
+   */
+  mapBannerSize(classes) {
+    const sizeRegex = /size_\w*/
+    if (classes.match(sizeRegex)) {
+      const size = classes.match(sizeRegex)[0].split('_').pop()
+      this.addToClassList(this.node, `${size}-section`)
+    }
+  }
+  /**
+   * Add CSS styles to node
+   * @param {string} property - CSS property name
+   * @param {string} value - CSS property value
+   */
+  css(property, value) {
+    this.node.style[property] = value
+  }
+  /**
+   * Add classes to a specific node
+   * @param {*} node
+   * @param {string} classes
+   */
+  addToClassList(node, ...classes) {
+    classes.forEach(c => node.classList.add(c))
+  }
+  /**
+   * Build DOM structure for banner component
+   */
+  buildDOM() {
+    this.node.removeChild(this.node.querySelector('h1'))
+    const classes = this.node.getAttribute('class')
+    this.resetAttr(this.node)
+
+    // Regex for evaluations
+    const multiRegex = /multi/
+    const singleRegex = /single/
+
+    this.mapBgColor(classes)
+    this.mapBannerSize(classes)
+
+    // Define columns, if any
+    if (classes.match(singleRegex)) {
+      this.addToClassList(this.node, 'column', 'centered')
+      const title = this.node.querySelector('.level2').querySelector('h2').textContent
+      const subtitle = this.node.querySelector('.level3').querySelector('h3').textContent
+      const link = this.node.querySelector('a').outerHTML
+      // Set image as background, if any
+      const image = this.node.querySelector('img')
+      if (image) {
+        const src = this.node.querySelector('img').getAttribute('src')
+        this.css('background', `url(${src}) no-repeat center center`)
+        this.css('backgroundSize', 'cover')
+      }
+      const template = `
+        ${title ? `<h1>${title}</h1>` : ''}
+        ${subtitle ? `<h3>${subtitle}</h3>` : ''}
+        ${link ? `${link}` : ''}
+      `
+      this.node.innerHTML = this.textToDOM(template)
+    } else if (classes.match(multiRegex)) {
+      this.addToClassList(this.node, 'grid')
+      // Add center classes to columns
+      Array.from(this.node.querySelectorAll('[class*=col]')).forEach(col => {
+        const colsRegex = /col-\w*/
+        const column = col.getAttribute('class').match(colsRegex)[0]
+        this.resetAttr(col)
+        this.addToClassList(col, 'column', 'centered', column)
+      })
+    }
+  }
 }
 
 // Banner
 const banners = Array.from(document.querySelectorAll('[id^=banner]'))
-console.log(banners)
-
-function mapBannerSize (size) {
-  return `${size}-section`
-}
-
-function mapBgColor (color) {
-  return `#${color}`
-}
-
-banners.forEach(banner => {
-  // Get classes
-  const classes = banner.getAttribute('class')
-
-  // Remove h1 by default
-  banner.removeChild(banner.querySelector('h1'))
-
-  // Regex for evaluations
-  const bgRegex = /bg_\w*/
-  const sizeRegex = /size_\w*/
-  const multiRegex = /multi/
-  const singleRegex = /single/
-  const colsRegex = /col-\w*/g
-  
-  // Reset classes
-  banner.setAttribute('class', '')
-
-  // Search for background color, if any
-  if (classes.match(bgRegex)) {
-    const color = mapBgColor(classes.match(bgRegex)[0].split('_').pop())
-    // Apply background color
-    banner.style.backgroundColor = color;
-  }
-  
-  // Search banner size, if any
-  if (classes.match(sizeRegex)) {
-    const size = classes.match(sizeRegex)[0].split('_').pop()
-    banner.classList.add(mapBannerSize(size))
-  }
-
-  // Define columns, if any
-  if (classes.match(singleRegex)) {
-    banner.classList.add('column', 'centered')
-    const title = banner.querySelector('.level2').querySelector('h2').textContent
-    const subtitle = banner.querySelector('.level3').querySelector('h3').textContent
-    const link = banner.querySelector('a').outerHTML
-    // Set image as background, if any
-    const image = banner.querySelector('img')
-    if (image) {
-      const src = banner.querySelector('img').getAttribute('src')
-      banner.style.background = `url(${src}) no-repeat center center`
-    }
-    const template = `
-      ${title ? `<h1>${title}</h1>` : ''}
-      ${subtitle ? `<h3>${subtitle}</h3>` : ''}
-      ${link ? `${link}` : ''}
-    `
-    banner.innerHTML = textToDom(template)
-  } else if (classes.match(multiRegex)) {
-    banner.classList.add('grid')
-    // Add center classes to columns
-    Array.from(banner.querySelectorAll('[class*=col]')).forEach(col => col.classList.add('column', 'centered'))
-  }
-
-  // Remove id attr
-  // banner.removeAttribute('id')
-  // // Create columns
-  // if (classes.match(/col-\w*/g)) {
-  //   const columns = classes.match(/col-\w*/g).map(col => {
-  //     const column = document.createElement('div')
-  //     column.classList.add(col, 'column', 'centered')
-  //     return column
-  //   })
-  //   console.log(columns)
-  // }
-})
+banners.forEach(banner => new Banner(banner).buildDOM())
