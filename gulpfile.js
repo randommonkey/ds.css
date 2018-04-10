32f03e85a4901b2a3b33b7f160cbc4cd6f174579
David Daza <03dazal@gmail.com>
2018-04-10 10:11:55 -0500

    Add task for transpile JS code using babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const autoprefixer = require('gulp-autoprefixer')
+const babel = require('gulp-babel')
 const gulp = require('gulp')
 const pump = require('pump')
 const sass = require('gulp-sass')
+const uglify = require('gulp-uglify')
 
 gulp.task('sass', function (cb) {
   pump([
@@ -14,8 +16,18 @@ gulp.task('sass', function (cb) {
   ], cb)
 })
 
-gulp.task('watch', function () {
-  gulp.watch('./src/sass/**/*.scss', ['sass'])
+gulp.task('js', function (cb) {
+  pump([
+    gulp.src('./src/js/ds.js'),
+    babel(),
+    uglify(),
+    gulp.dest('./dist/')
+  ], cb)
 })
 
-gulp.task('default', ['sass'])
+gulp.task('watch', function () {
+  gulp.watch('./src/sass/**/*.scss', ['sass'])
+  gulp.watch('./src/js/*.js', ['js'])
+})
+
+gulp.task('default', ['sass', 'js'])
