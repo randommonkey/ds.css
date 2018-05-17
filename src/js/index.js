95ddba8f54fa12a959ca74efc74e96d3121bb194
David Daza <03dazal@gmail.com>
2018-05-17 10:31:38 -0500

    Add nav key in global function. Not available in production

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import Banner from '@/components/Banner'
 import Box from '@/components/Box'
 import Carousel from '@/components/Carousel'
 import Iframe from '@/components/Iframe'
+import Nav from '@/components/Nav'
 import Overlay from '@/components/Overlay'
 import Search from '@/components/Search'
 import SplittedText from '@/components/SplittedText'
@@ -18,17 +19,26 @@ import overlayHandler from 'Utilities/overlay'
 import merge from 'deepmerge'
 const WebFont = require('webfontloader')
 
-const defaultOpts = {
+const defaults = {
   fonts: {
     title: {
       fallback: 'sans-serif',
-      family: 'Open Sans',
-      size: 20
+      family: 'Open Sans'
     },
     body: {
       fallback: 'serif',
-      family: 'Prata',
-      size: 16
+      family: 'Prata'
+    }
+  },
+  nav: {
+    mode: 'fixed', // static
+    links: [
+      { label: 'Inicio', href: 'index' },
+      { label: 'Contacto', href: 'contact' }
+    ],
+    logo: {
+      image: 'https://www.datasketch.co/images/ds-horizontal-pink.png',
+      href: 'https://www.datasketch.co/es'
     }
   }
 }
@@ -47,42 +57,52 @@ function injectCSS (css) {
   head.appendChild(style)
 }
 
+window.titiInstance = null
+
 window.titi = function titi (opts = {}) {
-  const config = merge(defaultOpts, opts)
+  if (!window.titiInstance) {
+    window.titiInstance = true
+    const config = merge(defaults, opts)
 
-  // Load fonts
-  const { fonts } = config
-  WebFont.load({
-    google: {
-      families: [fonts.title.family, fonts.body.family]
-    }
-  })
+    // Load fonts
+    const { fonts } = config
+    WebFont.load({
+      google: {
+        families: [fonts.title.family, fonts.body.family]
+      }
+    })
 
-  // Build css
-  const css = `
-    body {
-      font-family: "${fonts.body.family}", ${fonts.body.fallback};
-    }
-    h1, h2, h3, h4, h5, h6 {
-      font-family: "${fonts.title.family}", ${fonts.title.fallback};
-    }
-  `
-  // Inject generated css in head
-  injectCSS(css)
+    // Build css
+    const css = `
+      body {
+        font-family: "${fonts.body.family}", ${fonts.body.fallback};
+      }
+      h1, h2, h3, h4, h5, h6 {
+        font-family: "${fonts.title.family}", ${fonts.title.fallback};
+      }
+    `
+    // Inject generated css in head
+    injectCSS(css)
 
-  // Apply text color
-  Array.from(document.body.getElementsByTagName('*')).forEach(element => new Utilities(element))
+    // Build nav
+    new Nav(config.nav).buildDOM()
 
-  // Initialize components
-  initializeComponent(Banner, 'banner')
-  initializeComponent(Box, 'boxes')
-  initializeComponent(Carousel, 'carousel')
-  initializeComponent(Iframe, 'iframe')
-  initializeComponent(Search, 'search')
-  initializeComponent(SplittedText, 'splitted')
-  initializeComponent(Text, 'text')
+    // Apply text color
+    Array.from(document.body.getElementsByTagName('*')).forEach(element => new Utilities(element))
 
-  // Initialize overlay
-  new Overlay(document.querySelector('#overlays')).buildDOM()
-  overlayHandler()
+    // Initialize components
+    initializeComponent(Banner, 'banner')
+    initializeComponent(Box, 'boxes')
+    initializeComponent(Carousel, 'carousel')
+    initializeComponent(Iframe, 'iframe')
+    initializeComponent(Search, 'search')
+    initializeComponent(SplittedText, 'splitted')
+    initializeComponent(Text, 'text')
+
+    // Initialize overlay
+    new Overlay(document.querySelector('#overlays')).buildDOM()
+    overlayHandler()
+    return
+  }
+  throw new Error('Instance of titi already called')
 }
