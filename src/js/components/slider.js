2f8e9e7fe6562c89e1f777fd8cc228b3edfffcf3
David Daza <03dazal@gmail.com>
2018-12-07 12:16:20 -0500

    Add config options and style to slider component

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -2,39 +2,61 @@ import Swiper from 'swiper'
 
 const sliders = [...document.querySelectorAll('.slider')]
 
-const addPagination = options => {
-  return {
-    ...options,
-    pagination: {
-      clickable: true,
-      el: '.swiper-pagination'
-    }
-  }
+const hasAttribute = (slider, attr) => {
+  return slider.hasAttribute(attr)
 }
 
-const addNavigation = options => {
-  return {
-    ...options,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
+const enablePagination = (slider, options) => {
+  return !hasAttribute(slider, 'data-pagination')
+    ? options
+    : {
+      ...options,
+      pagination: {
+        clickable: true,
+        el: '.swiper-pagination'
+      }
     }
-  }
 }
 
-const addLoop = options => {
-  return {
-    ...options,
-    loop: true
-  }
+const enableNavigation = (slider, options) => {
+  return !hasAttribute(slider, 'data-navigation')
+    ? options
+    : {
+      ...options,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      }
+    }
+}
+
+const enableLoop = (slider, options) => {
+  return !hasAttribute(slider, 'data-loop')
+    ? options
+    : {
+      ...options,
+      loop: true
+    }
+}
+
+const enableAutoplay = (slider, options) => {
+  return !hasAttribute(slider, 'data-autoplay')
+    ? options
+    : {
+      ...options,
+      autoplay: {
+        delay: Number.parseFloat(slider.getAttribute('data-autoplay')) * 1000 || 3000
+      }
+    }
 }
 
 if (sliders) {
   for (const slider of sliders) {
-    let options = { grabCursor: true }
-    options = slider.hasAttribute('data-pagination') ? addPagination(options) : options
-    options = slider.hasAttribute('data-navigation') ? addNavigation(options) : options
-    options = slider.hasAttribute('data-loop') ? addLoop(options) : options
+    let options = { grabCursor: true, preloadImages: false, lazy: true }
+    options = enablePagination(slider, options)
+    options = enableNavigation(slider, options)
+    options = enableLoop(slider, options)
+    options = enableAutoplay(slider, options)
     new Swiper(slider, options) /* eslint no-new: off */
   }
 }
