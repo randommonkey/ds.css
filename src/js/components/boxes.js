3411603491ab3095ccc9591f548e87b18f1ce634
David Daza <03dazal@gmail.com>
2018-12-17 17:55:36 -0500

    Change post list proportions

--- src/js/components/boxes.js
+++ src/js/components/boxes.js
@@ -2,13 +2,13 @@ const boxItems = [...document.querySelectorAll('.box')]
 const boxes = [...document.querySelectorAll('.boxes')]
 
 const setBoxItemClasses = item => {
-  const boxesOnMobile = Number.parseInt(item.getAttribute('data-mobile')) || 1
+  const boxesOnMobile = Number.parseInt(item.getAttribute('data-mobile')) || 0
   const boxesOnTablet = Number.parseInt(item.getAttribute('data-tablet')) || boxesOnMobile
   const boxesOnPc = Number.parseInt(item.getAttribute('data-pc')) || boxesOnTablet
 
-  item.classList.add(`mobile-${boxesOnMobile}`)
-  item.classList.add(`tablet-${boxesOnTablet}`)
-  item.classList.add(`pc-${boxesOnPc}`)
+  boxesOnMobile ? item.classList.add(`mobile-${boxesOnMobile}`) : item.classList.add('mobile')
+  boxesOnTablet ? item.classList.add(`tablet-${boxesOnTablet}`) : item.classList.add('tablet')
+  boxesOnPc ? item.classList.add(`pc-${boxesOnPc}`) : item.classList.add('pc')
 
   item.hasAttribute('data-mobile') && item.removeAttribute('data-mobile')
   item.hasAttribute('data-tablet') && item.removeAttribute('data-tablet')
@@ -16,8 +16,10 @@ const setBoxItemClasses = item => {
 }
 
 const setBoxClasses = box => {
-  box.hasAttribute('data-justify') && box.classList.add(`justify-to-${box.getAttribute('data-justify')}`)
+  box.hasAttribute('data-justify') && box.classList.add(`justify-${box.getAttribute('data-justify')}`)
   box.hasAttribute('data-align') && box.classList.add(`align-to-${box.getAttribute('data-align')}`)
+  box.hasAttribute('data-justify') && box.removeAttribute('data-justify')
+  box.hasAttribute('data-align') && box.removeAttribute('data-align')
 }
 
 if (boxItems) {
