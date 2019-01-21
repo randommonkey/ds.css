f6e5000a6ed3b0543b29ef6219f17c7cda3477e4
David Daza <03dazal@gmail.com>
2019-01-21 10:35:18 -0500

    Add highlight.js to syntax highlighting in code blocks

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,18 @@
 import 'swiper/dist/css/swiper.min.css'
+import 'highlight.js/styles/monokai.css'
 import '../sass/titi.scss'
-import '../js/components/nav'
-import '../js/components/iframe'
-import '../js/components/slider'
 import '../js/components/boxes'
+import '../js/components/iframe'
+import '../js/components/nav'
+import '../js/components/slider'
+import hljs from 'highlight.js/lib/highlight'
+import bash from 'highlight.js/lib/languages/bash'
+import javascript from 'highlight.js/lib/languages/javascript'
+import nginx from 'highlight.js/lib/languages/nginx'
+import r from 'highlight.js/lib/languages/r'
+
+hljs.registerLanguage('bash', bash)
+hljs.registerLanguage('javascript', javascript)
+hljs.registerLanguage('nginx', nginx)
+hljs.registerLanguage('r', r)
+hljs.initHighlightingOnLoad()
